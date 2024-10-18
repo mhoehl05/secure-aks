29fc946006012b21f55ed7a1d94164c9186ffee1
Höhl <MHOEHL@controlware.de>
2024-10-18 12:19:28 +0200

    fixed references

--- identity.tf
+++ identity.tf
@@ -1,17 +1,17 @@
 resource "azurerm_user_assigned_identity" "aks_identity" {
   name                = "idcluster-secureaks-demo-weu"
-  resource_group_name = data.azurerm_resource_group.main.name
-  location            = data.azurerm_resource_group.main.location
+  resource_group_name = data.azurerm_resource_group.aks_rg.name
+  location            = data.azurerm_resource_group.aks_rg.location
 }
 
 resource "azurerm_role_assignment" "dns_contributor" {
   scope                = azurerm_private_dns_zone.aks_dns.id
   role_definition_name = "Private DNS Zone Contributor"
-  principal_id         = azurerm_user_assigned_identity.cluster_identity.principal_id
+  principal_id         = azurerm_user_assigned_identity.aks_identity.principal_id
 }
 
 resource "azurerm_role_assignment" "network_contributor" {
   scope                = azurerm_virtual_network.aks_vnet.id
   role_definition_name = "Network Contributor"
-  principal_id         = azurerm_user_assigned_identity.cluster_identity.principal_id
+  principal_id         = azurerm_user_assigned_identity.aks_identity.principal_id
 }
